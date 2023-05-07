8239b1f9f36d37f1c3ad401838b5afe14202ca5a
Abhishek Arora <mail2abhishekarora@gmail.com>
2023-05-07 19:40:32 +0530

    Changes against my work in about section

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -125,7 +125,7 @@ const About = () => {
     sr.reveal(revealContainer.current, srConfig());
   }, []);
 
-  const skills = ['JavaScript (ES6+)', 'TypeScript', 'React', 'Eleventy', 'Node.js', 'WordPress'];
+  const skills = ['JavaScript (ES6+)', 'TypeScript', 'React', 'Vue', 'Node.js', 'WordPress'];
 
   return (
     <StyledAboutSection id="about" ref={revealContainer}>
@@ -135,30 +135,34 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
+              Hey there, I'm Abhishek - a senior software ninja with more than 6 years of experience
+              in web development. I'm a React.js &amp; Vue.js aficionado who's crafted amazing
+              projects for the big guns in India's tech industry.
             </p>
 
             <p>
               Fast-forward to today, and I’ve had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
+              <a href="https://www.tanla.com" target="_blank" rel="noreferrer">
+                a cloud-communication provider
+              </a>
+              ,{' '}
+              <a href="https://gamooga.com" target="_blank" rel="noreferrer">
+                a start-up
+              </a>
+              , and{' '}
+              <a href="https://www.cognizant.com" target="_blank" rel="noreferrer">
+                a large-cap technical consultancy
+              </a>
+              . My main focus these days is building accessible, inclusive, world-class products,
+              while implementing scalable architecture patterns using higher order components.
             </p>
 
             <p>
-              I also recently{' '}
-              <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
-                launched a course
-              </a>{' '}
-              that covers everything you need to build a web app with the Spotify API using Node
-              &amp; React.
+              Recently, I launched a comprehensive career advancement package at{' '}
+              <a href="https://topmate.io/abhishekarora/283657">TopMate</a> , which includes
+              personalized career coaching, resume review, interview preparation, and networking
+              strategies. Aimed at helping job seekers secure their dream job in a competitive
+              market.
             </p>
 
             <p>Here are a few technologies I’ve been working with recently:</p>
